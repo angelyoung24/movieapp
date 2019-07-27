d696823a36c2ae10a16030e907715fe5b80ac248
Angel <angel.m.young24@gmail.com>
2019-07-27 17:52:37 -0400

    Updates

---
+++ precache-manifest.660fa2dca2d8e0547a2199c3ad7c557a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eaa80dc591a61e8dd3893457e31d120",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "86ca74024593648613b5",
+    "url": "/movieapp/static/css/main.776e49df.chunk.css"
+  },
+  {
+    "revision": "c1dd2af70c0e1bd9788b",
+    "url": "/movieapp/static/js/2.0d1aa941.chunk.js"
+  },
+  {
+    "revision": "86ca74024593648613b5",
+    "url": "/movieapp/static/js/main.866d1a8a.chunk.js"
+  },
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  }
+]);
