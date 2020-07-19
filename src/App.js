911355def7d72e1fedfe9747811732b17a817122
angelyoung24 <angel.m.young24@gmail.com>
2020-07-19 13:54:53 -0400

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import './App.css';
+import "./App.css";
 
 function Movie(props) {
-  return <img alt="movies" src = {props.src}/>;
+  return <img alt="movies" src={props.src} />;
 }
 
 function Movies(props) {
@@ -16,51 +16,47 @@ function Movies(props) {
       return;
     }
 
-    fetch(`https://www.omdbapi.com/?s=${props.search}&apikey=e2f1f9a0`).then(
-      response =>
-      response.json().then(json => {
-        setMovies(json.Search);
-        setLoading(false);
-      })
+    fetch(`https://www.omdbapi.com/?s=${props.search}&apikey=33d4bf5a`).then(
+      (response) =>
+        response.json().then((json) => {
+          setMovies(json.Search);
+          setLoading(false);
+        })
     );
   }, [props.search]);
 
   if (loading || !movies) {
-    return <div className="loading"> loading... < /div>;
+    return <div className="loading"> searching... </div>;
   }
 
-  return movies.map(movie => < Movie src = {
-      movie.Poster
-    }
-    />);
-  }
+  return movies.map((movie) => <Movie src={movie.Poster} />);
+}
 
-  function App() {
-    const [search, setSearch] = React.useState("");
-    return ( <div className = "App" >
+function App() {
+  const [search, setSearch] = React.useState("");
+  return (
+    <div className="App">
       <table>
         <tbody>
           <tr>
-              <td><h1 > Angel's Movie App</h1></td>
+            <td>
+              <h1> Angel's Movie App</h1>
+            </td>
           </tr>
         </tbody>
-      
-      
-       <input placeholder="Enter Movie Name" value = {
-        search
-      }
-      onChange = {
-        e => setSearch(e.target.value)
-      }/> 
-      <Movies search = {
-        search
-          } />
-          </table>
-          </div>
-    );
-  }
 
-  const rootElement = document.getElementById("root");
-  ReactDOM.render( < App / > , rootElement);
+        <input
+          placeholder="Enter Movie Name"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Movies search={search} />
+      </table>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
